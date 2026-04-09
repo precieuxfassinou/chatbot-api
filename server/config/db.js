98e179ced8f15486d6d77fab70ab6cf82b99363a
Précieux Fassinou <precieuxfassinou@gmail.com>
2026-04-09 19:13:57 +0000

    fix database connexion

--- server/config/db.js
+++ server/config/db.js
@@ -3,12 +3,10 @@ const path = require("path");
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5435,
-    database: process.env.DB_NAME,
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
+
 
 pool.on('error', (err) => {
     console.error('Unexpected error on idle client', err);
